6882f13118c4f9e8e8a69e36f100dba7b89b68b9
Kay Cee <se.kaycee@gmail.com>
2023-02-07 05:33:11 +0100

    edit BaseModel

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+''' Create a unique FileStorage instance  '''
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 ''' BaseModel module.. form the baseclass for other project models '''
-
 from uuid import uuid4
-# from models import storage
+from models import storage
 from datetime import datetime
 
 
 class BaseModel:
     ''' Define all common attributes/methods '''
+
     def __init__(self, *args, **kwargs):
         ''' Deserialize a serialized class or initialize a new one '''
         if not kwargs:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -29,6 +30,7 @@ class BaseModel:
     def save(self):
         ''' Update `self.updated_at` with the current datetime '''
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         ''' Return a new dictionary with key and strformatted datetime '''
